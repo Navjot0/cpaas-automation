48daebfccc344e50055d2ba131e6362bc789e075
Navjot Singh <navjotsingh9495@gmail.com>
2026-02-22 00:42:43 +0530

    map variable updated

--- src/test/java/com/automation/pages/CreateCampaignPage.java
+++ src/test/java/com/automation/pages/CreateCampaignPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDate;
